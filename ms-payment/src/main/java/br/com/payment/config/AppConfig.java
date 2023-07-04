a71cea9ff0deeca0c07ee9c725106fcc471d1d0d
Spenezanni <gustavospenezanni@gmail.com>
2023-07-04 09:14:26 -0300

    Initial Project PayRoll

---
+++ ms-payment/src/main/java/br/com/payment/config/AppConfig.java
@@ -0,0 +1,10 @@
+package br.com.payment.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+	
+	
+	
+}
--- ms-worker/src/main/java/com/br/worker/MsWorkerApplication.java
+++ ms-worker/src/main/java/com/br/worker/MsWorkerApplication.java
@@ -1,4 +1,4 @@
-package com.br;
+package com.br.worker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
