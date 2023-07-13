ed10d728d69fa43e8a4d692fc6106eba771c6c85
Spenezanni <gustavospenezanni@gmail.com>
2023-07-13 08:15:48 -0300

    Integration Eureka - Gateway

--- api-gateway/src/main/java/gateway/com/br/ApiGatewayApplication.java
+++ api-gateway/src/main/java/gateway/com/br/ApiGatewayApplication.java
@@ -2,7 +2,9 @@ package gateway.com.br;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ApiGatewayApplication {
 
