dcaeeaada1a9873f1e494ac9a508e9bcf9ac331a
Spenezanni <gustavospenezanni@gmail.com>
2023-09-02 15:03:21 -0300

    Creating project MS-OAUTH Authorization Server

---
+++ ms-oauth/src/main/java/oauth/com/br/MsOauthApplication.java
@@ -0,0 +1,13 @@
+package oauth.com.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsOauthApplication.class, args);
+	}
+
+}
---
+++ ms-oauth/src/test/java/oauth/com/br/MsOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package oauth.com.br;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
